87ec584d2e365f2b2d1de6ce5af74872f5c676f4
pguidi <50623805+pguidi@users.noreply.github.com>
2019-05-14 16:28:50 -0300

    Add files via upload
    Adicionei pasta com códigos C

---
+++ TCA/Código C/AUTOLCT_02/AUTOLCT_02.ino
@@ -0,0 +1,28 @@
+
+
+void setup() {
+
+  Serial.begin(9600);
+  }
+
+void loop() {
+  
+char leitura;
+leitura = Serial.read();
+                              
+    if (leitura == '1') // USE MODO CENTRAL (ATU-C)
+  {
+    Serial.println("Digitou 1"); 
+  }
+   else
+   if (leitura == '2') // DESBILITA TODOS RELES (EXCETO RELE K)
+  {
+    Serial.println("Digitou 2"); 
+  }
+   else
+   if (leitura == '3') // DESBILITA TODOS RELES
+  {
+    Serial.println("Digitou 3"); 
+  } 
+
+}
---
+++ TCA/Código C/AUTOLCT_D_3/AUTOLCT_D_3.ino
@@ -0,0 +1,53 @@
+
+
+int porta4 = 4;
+int porta7 = 7;
+
+String readString;
+
+void setup() {
+
+  Serial.begin(9600);
+
+pinMode (porta4, OUTPUT);
+pinMode (porta7, OUTPUT);
+  //pinMode (rele_v, OUTPUT); // k2
+  //pinMode (rele_x, OUTPUT); // k3
+  }
+
+void loop() {
+  
+  while (Serial.available()) {
+    delay(3);  
+    char c = Serial.read();
+    readString += c; 
+  }
+  if (readString.length() >0) {
+    
+      if (readString == "1") {
+             
+      Serial.print("habilitado");
+      digitalWrite(porta4,HIGH);
+      digitalWrite(porta7,HIGH);
+      //delay(700);
+      //digitalWrite(porta4,LOW);
+      //digitalWrite(porta7,LOW);
+      
+  }
+    
+      if (readString == "2") {
+             
+      Serial.print("desabilitado");
+      digitalWrite(porta4,LOW);
+      digitalWrite(porta7,LOW);
+      //delay(700);
+      //digitalWrite(porta4,LOW);
+      //digitalWrite(porta7,LOW);
+      
+  }
+
+
+
+     readString="";
+  } 
+}
